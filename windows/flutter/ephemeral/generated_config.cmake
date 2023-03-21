58a5752e044f123b195ce65fcf6d074671f6f1e8
Sumit Sonar <90107230+Sumit-Sonar@users.noreply.github.com>
2023-03-22 01:21:48 +0530

    instagram_ui_clone-flutter
    in this project, i was on my flutter UI designing stage so i have developed an instagram UI clone app in flutter.

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Sumit Sonar\\Flutter Projects\\instagram_ui_clone" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Sumit Sonar\\Flutter Projects\\instagram_ui_clone"
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Sumit Sonar\\Flutter Projects\\instagram_ui_clone\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Sumit Sonar\\Flutter Projects\\instagram_ui_clone"
+  "FLUTTER_TARGET=C:\\Sumit Sonar\\Flutter Projects\\instagram_ui_clone\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Sumit Sonar\\Flutter Projects\\instagram_ui_clone\\.dart_tool\\package_config.json"
+)
